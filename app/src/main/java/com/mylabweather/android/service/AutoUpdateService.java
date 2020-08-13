b62cd03b8906a4dd56c1b73c10db5659d0bcac8a
Max <max@msyher.com>
2020-08-13 16:51:38 +0800

    修改程序图标和名称。

--- app/src/main/java/com/mylabweather/android/service/AutoUpdateService.java
+++ app/src/main/java/com/mylabweather/android/service/AutoUpdateService.java
@@ -50,7 +50,7 @@ public class AutoUpdateService extends Service {
             // 有缓存时直接解析天气数据
             Weather weather = Utility.handleWeatherResponse(weatherString);
             String weatherId = weather.basic.weatherId;
-            String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=bc0418b57b2d4918819d3974ac1285d9";
+            String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=896d187a33584c0a99fbad54ae4a19be";
             HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
                 @Override
                 public void onResponse(Call call, Response response) throws IOException {
