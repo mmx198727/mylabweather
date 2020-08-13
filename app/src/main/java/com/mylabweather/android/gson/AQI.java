f6fb86ada847622541b9eaa8bf8754b16085c970
Max <max@msyher.com>
2020-08-13 15:41:22 +0800

    加入显示天气信息功能。

---
+++ app/src/main/java/com/mylabweather/android/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.mylabweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
+
--- app/src/main/java/com/mylabweather/android/util/Utility.java
+++ app/src/main/java/com/mylabweather/android/util/Utility.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.mylabweather.android.db.City;
 import com.mylabweather.android.db.County;
 import com.mylabweather.android.db.Province;
+import com.mylabweather.android.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,6 +80,21 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
